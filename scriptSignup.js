3fa9f81a8c53309fd07ce94f07b93838f648f10d
noor-ta <noortaerq2004@gmail.com>
2025-07-10 17:34:03 +0300

    Add files via upload

---
+++ scriptSignup.js
@@ -0,0 +1,19 @@
+//  Conditionals (if-else statements)
+
+var age = prompt("Please, Enter your age ... ");
+
+if (age >= 18) {
+  alert("You are eligible to sign up.");
+} else {
+  alert("You must be at least 18 years old to sign up.");
+}
+
+//  Form Validation
+
+function validateForm() {
+  var email = document.getElementById("email").value;
+
+  if (email === "") {
+    alert("Please fill in the email field.");
+  }
+}
